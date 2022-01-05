15ca46b169f086a7c4051fa59236972afba3fe07
Dwight <dwight@Marketing-1-iMac.local>
2022-01-05 16:55:22 -0500

    added labels

--- src/App.js
+++ src/App.js
@@ -6,29 +6,36 @@ function App() {
     <div className="App">
       <div className="container">
       <div className="row">
+      <p>&lt;Button /&gt;</p>        
         <Button>Default</Button>
       </div>
       <div className="row">
+      <p>&lt;Button variant="outline" /&gt;</p>        
         <Button variant="outline">Default</Button>
       </div>
       <div className="row">
+      <p>&lt;Button variant="text" /&gt;</p>        
         <Button variant="text">Default</Button>
       </div>
       <div className="row">
+      <p>&lt;Button variant="disableShadow" /&gt;</p>        
         <Button variant="disableShadow">Default</Button>
       </div>
       <div className="row">
+      <p>&lt;Button variant="disabled" /&gt;</p>        
         <Button variant="disabled">Default</Button>
       </div>
       <div className="multi-row">
         <div className="multi-row-item">
-        <Button startIcon="local_grocery_store">
+        <p>&lt;Button startIcon=”local_grocery_store” /&gt;</p>        
+        <Button color="primary" startIcon="local_grocery_store">
           <span class="material-icons">add_shopping_cart</span>
           Default
         </Button>
         </div>
         <div className="multi-row-item">
-        <Button endIcon="local_grocery_store">
+        <p>&lt;Button endIcon=”local_grocery_store” /&gt;</p>        
+        <Button color="primary" endIcon="local_grocery_store">
           Default
           <span class="material-icons">add_shopping_cart</span>
         </Button>
