9912a2fbacafc385b32f7098b9709a2a152d2281
Dwight Carter <bigbrother@dwights-mbp.home>
2022-01-06 18:24:12 -0500

    added more css

--- src/App.js
+++ src/App.js
@@ -5,74 +5,84 @@ function App() {
   return (
     <div className="App">
       <div className="container">
+        <div className="row">
+          <h1>Buttons</h1>
+        </div>
       <div className="row">
-      <p>&lt;Button /&gt;</p>        
+      <p className="label">&lt;Button /&gt;</p>        
         <Button>Default</Button>
       </div>
       <div className="row">
-      <p>&lt;Button variant="outline" /&gt;</p>        
+      <p className="label">&lt;Button variant="outline" /&gt;</p>        
         <Button variant="outline">Default</Button>
       </div>
       <div className="row">
-      <p>&lt;Button variant="text" /&gt;</p>        
+      <p className="label">&lt;Button variant="text" /&gt;</p>        
         <Button variant="text">Default</Button>
       </div>
       <div className="row">
-      <p>&lt;Button variant="disableShadow" /&gt;</p>        
+      <p className="label">&lt;Button variant="disableShadow" /&gt;</p>        
         <Button variant="disableShadow">Default</Button>
       </div>
       <div className="row">
-      <p>&lt;Button variant="disabled" /&gt;</p>        
+      <p className="label">&lt;Button variant="disabled" /&gt;</p>        
         <Button variant="disabled">Default</Button>
       </div>
+      <div className="row">
+      <p className="label">&lt;Button variant="disabled" /&gt;</p>        
+        <Button disabled variant="text">Default</Button>
+      </div>
       <div className="multi-row">
         <div className="multi-row-item">
-        <p>&lt;Button startIcon=”local_grocery_store” /&gt;</p>        
-        <Button color="primary" startIcon="local_grocery_store">
-          <span class="material-icons">add_shopping_cart</span>
+        <p className="label">&lt;Button starticon=”local_grocery_store” /&gt;</p>        
+        <Button color="primary" starticon="local_grocery_store">
+          <span className="material-icons">add_shopping_cart</span>
           Default
         </Button>
         </div>
         <div className="multi-row-item">
-        <p>&lt;Button endIcon=”local_grocery_store” /&gt;</p>        
-        <Button color="primary" endIcon="local_grocery_store">
+        <p className="label">&lt;Button endicon=”local_grocery_store” /&gt;</p>        
+        <Button color="primary" endicon="local_grocery_store">
           Default
-          <span class="material-icons">add_shopping_cart</span>
+          <span className="material-icons">add_shopping_cart</span>
         </Button>
         </div>
       </div>
       <div className="multi-row">
         <div className="multi-row-item">
-        <p>&lt;Button size=sm /&gt;</p>        
+        <p className="label">&lt;Button size=sm /&gt;</p>        
         <Button size="sm">Default</Button>
         </div>
         <div className="multi-row-item">
-        <p>&lt;Button size=md /&gt;</p>        
+        <p className="label">&lt;Button size=md /&gt;</p>        
         <Button size="md">Default</Button>
         </div>
         <div className="multi-row-item">
-        <p>&lt;Button size=lg /&gt;</p>        
+        <p className="label">&lt;Button size=lg /&gt;</p>        
         <Button size="lg">Default</Button>
         </div>
       </div>
       <div className="multi-row">
         <div className="multi-row-item"> 
-        <p>&lt;Button color=”default” /&gt;</p>        
+        <p className="label">&lt;Button color=”default” /&gt;</p>        
       <Button color="default">Default</Button>
         </div>
         <div className="multi-row-item">
-        <p>&lt;Button color=primary /&gt;</p>        
+        <p className="label">&lt;Button color=primary /&gt;</p>        
         <Button color="primary">Primary</Button>
         </div>
         <div className="multi-row-item">
-        <p>&lt;Button color=secondary /&gt;</p>        
+        <p className="label">&lt;Button color=secondary /&gt;</p>        
         <Button color="secondary">Secondary</Button>
         </div>
         <div className="multi-row-item">
-        <p>&lt;Button color=danger /&gt;</p>        
+        <p className="label">&lt;Button color=danger /&gt;</p>        
         <Button color="danger">Danger</Button>          
         </div>
       </div>
+      <div className="row footer">
+          <p> created by <span className="spanName">Dwightx</span> - devChallenges.io</p>
+        </div>
       </div>
     </div>
   );
--- src/components/Buttom.jsx
+++ src/components/Buttom.jsx
@@ -8,13 +8,13 @@ import '../typography.css';
 // color: the selected color
 // children: the node passed inside the Component
 // ...props: the default attribute of the Component
-const Button = ({ variant,startIcon,endIcon,children,size,color}) => {
+const Button = ({ variant,starticon,endicon,children,size,color,disabled}) => {
 // If the variant exists in variantsMapping, we use it. 
 // Otherwise, use p tag instead.
 
   return (
     <button
-      className={variant || size || color} startIcon={startIcon} endIcon={endIcon}>
+      className={variant || size || color} starticon={starticon} endicon={endicon} disabled={disabled}>
       {children}
     </button>
   );
